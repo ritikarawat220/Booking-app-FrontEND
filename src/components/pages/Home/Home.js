9a664db7939cdc366e71b6bc8b20113cda7b412a
Ritika Rawat <Ritikarawat220@gmail.com>
2023-08-16 16:35:30 +0530

    Add home page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,3 +1,37 @@
-const Home = () => <h1>This is the MODELS page</h1>;
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Home.css';
+import img from '../../../assets/img.jpg';
+
+const Airplane = ({ model, description }) => (
+  <>
+    <div className="vespa_container">
+      <div className="vespa-circle">
+        <img className="vespas" src={img} alt="" />
+      </div>
+      <h3 className="vespa-model">{model.toUpperCase()}</h3>
+      <p className="vespa-description">{description}</p>
+    </div>
+  </>
+);
+
+Airplane.propTypes = {
+  model: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
+const Home = () => (
+  <section className="homepage">
+    <div className="hero">
+      <h1 className="hero_header">LATEST MODELS</h1>
+      <p className="hero_text">Please select a Vespa Model</p>
+      <div className="vehicle-container">
+        {[1, 2, 3].map((i) => (
+          <Airplane key={i} model="Vespa C20" description="The Vespa C20 is such a unique model that it has been known for longevity." />
+        ))}
+      </div>
+    </div>
+  </section>
+);
 
 export default Home;
