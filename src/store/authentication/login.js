2146030bb701e6c11e4b67d51a7c8c843e5a3bc6
Rana-Mudassir <mduasire38@gmail.com>
2023-08-20 20:05:48 +0500

    Create Redux, store basic structure

---
+++ src/store/authentication/login.js
@@ -0,0 +1,13 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  //somevalue
+};
+
+const loginSlice = createSlice({
+  name: 'name',
+  initialState,
+  reducers:{},
+});
+
+export default loginSlice.reducer;
