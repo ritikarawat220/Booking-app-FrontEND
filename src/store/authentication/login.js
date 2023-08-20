01b412e2f5b0597eddda0db67e5d105e973f89dd
Rana-Mudassir <mduasire38@gmail.com>
2023-08-21 01:20:57 +0500

    Add Login Slice

--- src/store/authentication/login.js
+++ src/store/authentication/login.js
@@ -1,16 +1,42 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  // somevalue
+  loading: false,
+  error: null,
+  user: null,
+  token: null,
 };
 
-const login = createAsyncThunk();
-console.log('login', login);
+export const loginAsync = createAsyncThunk('login/loginAsync', async (credentials) => {
+  const res = await fetch('http//xyz.com', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  });
+
+  if (!res.ok) {
+    const errData = await res.json();
+    throw new Error(errData.message);
+  }
+  const data = await res.json();
+  return data.token;
+});
 
 const loginSlice = createSlice({
-  name: 'name',
+  name: 'login',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginAsync.pending, (state) => (
+        { ...state, loading: true, error: null }))
+      .addCase(loginAsync.fulfilled, (state, action) => (
+        { ...state, loading: false, token: action.payload }))
+      .addCase(loginAsync.rejected, (state, action) => (
+        { ...state, loading: false, error: action.error.message }));
+  },
 });
 
 export default loginSlice.reducer;
