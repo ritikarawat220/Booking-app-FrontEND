8d840f30d20c720dffa9becf28462e5a2b5aeeb7
Rana-Mudassir <mduasire38@gmail.com>
2023-08-22 15:59:53 +0500

    update Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './authentication/login';
+import signupReducer from './authentication/signup';
 
 const store = configureStore({
   reducer: {
     login: loginReducer,
+    signup: signupReducer,
   },
 });
 
