7a34e8e0440449c2b5fb47e27927e96945a5bbf5
Rana-Mudassir <mduasire38@gmail.com>
2023-08-22 16:00:23 +0500

    update login slice

--- src/store/authentication/login.js
+++ src/store/authentication/login.js
@@ -11,17 +11,30 @@ export const loginAsync = createAsyncThunk('login/loginAsync', async (credential
   const res = await fetch('http://127.0.0.1:4000/login', {
     method: 'POST',
     headers: {
-      'content-type': 'application/json',
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(credentials),
+    body: JSON.stringify({
+      user: {
+        email: credentials.email,
+        password: credentials.password,
+      },
+    }),
   });
 
   if (!res.ok) {
     const errData = await res.json();
     throw new Error(errData.message);
   }
+
   const data = await res.json();
-  return data.token;
+  const authToken = res.headers.get('Authorization');
+  const user = data.data;
+
+  if (authToken) {
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('authToken', authToken);
+  }
+  return { data };
 });
 
 const loginSlice = createSlice({
@@ -33,7 +46,7 @@ const loginSlice = createSlice({
       .addCase(loginAsync.pending, (state) => (
         { ...state, loading: true, error: null }))
       .addCase(loginAsync.fulfilled, (state, action) => (
-        { ...state, loading: false, token: action.payload }))
+        { ...state, loading: false, token: action.payload.data }))
       .addCase(loginAsync.rejected, (state, action) => (
         { ...state, loading: false, error: action.error.message }));
   },
