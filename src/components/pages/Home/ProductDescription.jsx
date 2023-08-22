d3a81685cf5d42df1b4ff86ffeb2ccb0bde1283f
Ritika Rawat <Ritikarawat220@gmail.com>
2023-08-22 14:08:10 +0530

    Add styling for aeroplane description

--- src/components/pages/Home/ProductDescription.jsx
+++ src/components/pages/Home/ProductDescription.jsx
@@ -1,13 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { TfiArrowCircleRight } from 'react-icons/tfi';
 import { fetchProductDescription } from '../../../store/productDescription/productDescriptionSlice';
+import './ProductDescription.css';
 
 export default function ProductDescription() {
   const dispatch = useDispatch();
-
   const { id } = useParams();
-
   const { productDescription, isLoading } = useSelector((store) => store.productDescription);
 
   useEffect(() => {
@@ -20,43 +20,42 @@ export default function ProductDescription() {
 
   return (
     <>
-      <div className="heading text-center">
-        <h3>
-          Product Details
-          {' '}
-          {id}
-
-        </h3>
-      </div>
-
       {productDescription.map((element) => (
-
-        <div key={element.id} className=" container-md display-flex justify-content-center">
-          <div className="display-flex justify-content-center">
+        <div key={element.id} className="detail-container">
+          <div className="details-image-container">
             <img src={element.image} alt={element.name} />
           </div>
-          <div className="container-airp-details">
-            <li>
-              <div>Brand Name</div>
-              <div>{element.name}</div>
-            </li>
-            <li />
-            <li>
-              <div>Description</div>
-              <div>{element.description}</div>
-            </li>
-            <li>
-              <div>Model</div>
-              <div>{element.model}</div>
-            </li>
-            <li>
-              <div>Price</div>
-              <div>{element.price}</div>
-            </li>
-            <li>
-              <div>Booking Price</div>
-              <div>{element.Booking_Price}</div>
-            </li>
+          <div className="details-content-container">
+            <h3>{element.model}</h3>
+
+            <div className="details-description">
+              <p>
+                {element.description ? `${element.description.slice(0, 100)}...` : ''}
+              </p>
+            </div>
+
+            <ul className="vehicle-info-details">
+              <li>
+                <p>Name:</p>
+                {' '}
+                <p className="vehicle-info-data">{element.engine}</p>
+              </li>
+              <li>
+                <p>Model:</p>
+                {' '}
+                <p className="vehicle-info-data">{element.mileage}</p>
+              </li>
+              <li>
+                <p>Price:</p>
+                {' '}
+                <p className="vehicle-info-data">{element.price}</p>
+              </li>
+              <div className="reserve-vehicle">
+                <p>Reserve</p>
+                {' '}
+                <TfiArrowCircleRight className="arrow-right-reserve" />
+              </div>
+            </ul>
           </div>
         </div>
       ))}
