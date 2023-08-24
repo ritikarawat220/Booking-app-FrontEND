192064a0bea8688f5736c5c3a3c21a9b66c10a8d
FrankMugagga <frank.mugagga@gmail.com>
2023-08-24 17:49:52 +0300

    Add the test folder

---
+++ src/_tests_/Login.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from 'react-test-renderer'
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from '../store/store';
+import Login from '../components/pages/registration/Login'
+
+it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Router>
+            <Login />
+          </Router>
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
