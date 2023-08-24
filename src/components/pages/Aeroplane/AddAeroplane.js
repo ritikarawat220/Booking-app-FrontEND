9a0ac0d418026f75d493064de398e0ebb80cfda7
Rana-Mudassir <mduasire38@gmail.com>
2023-08-24 14:14:22 +0500

    Add Aeroplane componenet

--- src/components/pages/Aeroplane/AddAeroplane.js
+++ src/components/pages/Aeroplane/AddAeroplane.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addAeroplane } from '../../../store/aeroplane/aeroplane';
-
+/* eslint-disable */ 
 const AddAeroplane = () => {
   const dispatch = useDispatch();
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [model, setModel] = useState('');
   const [price, setPrice] = useState('');
-  const [bookingprice, setBookingprice] = useState('');
+  const [booking_price, setBookingprice] = useState('');
   const [image, setImage] = useState('');
 
-  const handleAddAeroplane = () => {
-    dispatch(addAeroplane({ name, description, price }));
+  const handleAddAeroplane = (e) => {
+    e.preventDefault();
+    dispatch(addAeroplane({
+      name, description, price, model, booking_price, image,
+    }));
     setName('');
     setDescription('');
     setModel('');
@@ -27,11 +30,11 @@ const AddAeroplane = () => {
       <form onSubmit={handleAddAeroplane}>
         <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
         <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
-        <input type="text" placeholder="<Model" value={model} onChange={(e) => setModel(e.target.value)} />
+        <input type="text" placeholder="Model" value={model} onChange={(e) => setModel(e.target.value)} />
         <input type="number" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} />
-        <input type="number" placeholder="Booking-Price" value={bookingprice} onChange={(e) => setBookingprice(e.target.value)} />
+        <input type="number" placeholder="Booking-Price" value={booking_price} onChange={(e) => setBookingprice(e.target.value)} />
         <input type="text" placeholder="Image Address" value={image} onChange={(e) => setImage(e.target.value)} />
-        {/* <button type="submit">Add</button> */}
+        <button type="submit" placeholder="Submit">Submit</button>
       </form>
     </div>
   );
--- src/store/aeroplane/aeroplane.js
+++ src/store/aeroplane/aeroplane.js
@@ -1,22 +1,25 @@
 import { createSlice, createAsyncThunk, createSelector } from '@reduxjs/toolkit';
 
-const API_BASE_URL = 'http://your-api-base-url.com';
+const API_BASE_URL = 'https://aeroplane-find.onrender.com';
 
 const initialState = {
   airplanes: [],
   status: 'idle',
   error: null,
+  loading: false,
 };
 
-const getTokenFromLocalStorage = () => localStorage.getItem('token');
+const getTokenFromLocalStorage = () => localStorage.getItem('authToken');
 
 export const addAeroplane = createAsyncThunk('aeroplanes/addAeroplane', async (aeroplaneData, { rejectWithValue }) => {
   try {
-    const response = await fetch(`${API_BASE_URL}/aeroplanes`, {
+    const authToken = localStorage.getItem('authToken');
+    console.log('token in aeroplane slice', authToken);
+    const response = await fetch(`${API_BASE_URL}/aeroplanes/create`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${getTokenFromLocalStorage()}`,
+        Authorization: `${authToken}`,
       },
       body: JSON.stringify(aeroplaneData),
     });
@@ -26,7 +29,9 @@ export const addAeroplane = createAsyncThunk('aeroplanes/addAeroplane', async (a
       return rejectWithValue(errorData);
     }
 
-    return response.json();
+    const newData = await response.json();
+    console.log('data aeplane', newData);
+    return newData;
   } catch (error) {
     return rejectWithValue(error.message);
   }
@@ -37,7 +42,7 @@ export const deleteAeroplane = createAsyncThunk('aeroplanes/deleteAeroplane', as
     const response = await fetch(`${API_BASE_URL}/aeroplanes/${aeroplaneId}`, {
       method: 'DELETE',
       headers: {
-        Authorization: `Bearer ${getTokenFromLocalStorage()}`,
+        Authorization: `${getTokenFromLocalStorage()}`,
       },
     });
 
@@ -65,28 +70,28 @@ const aeroplaneSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(addAeroplane.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(addAeroplane.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.airplanes.push(action.payload);
-      })
-      .addCase(addAeroplane.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.payload;
-      })
-      .addCase(deleteAeroplane.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(deleteAeroplane.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.airplanes = state.airplanes.filter((plane) => plane.id !== action.payload.id);
-      })
-      .addCase(deleteAeroplane.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.payload;
-      });
+      .addCase(addAeroplane.pending, (state) => (
+        { ...state, loading: true, error: null }))
+      .addCase(addAeroplane.fulfilled, (state, action) => ({
+        ...state,
+        loading: false,
+        airplanes: [...state.airplanes, action.payload],
+      }))
+      // .addCase(addAeroplane.fulfilled, (state, action) => (
+      //   { ...state, loading: false, airplanes: action.payload }))
+      .addCase(addAeroplane.rejected, (state, action) => (
+        { ...state, loading: false, error: action.error.message }));
+    // .addCase(deleteAeroplane.pending, (state) => {
+    //   state.status = 'loading';
+    // })
+    // .addCase(deleteAeroplane.fulfilled, (state, action) => {
+    //   state.status = 'succeeded';
+    //   state.airplanes = state.airplanes.filter((plane) => plane.id !== action.payload.id);
+    // })
+    // .addCase(deleteAeroplane.rejected, (state, action) => {
+    //   state.status = 'failed';
+    //   state.error = action.payload;
+    // });
   },
 });
 
